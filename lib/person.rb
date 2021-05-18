d7adf28efbc33d10cf99446de7fc3ff9a903fbec
Gabriela johnson <johnsongabriela12@gmail.com>
2021-05-18 20:46:55 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  
+  def name =(new_name)
+    @name = name 
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def job =(my_job)
+    @breed = my_job
+  end
+
+  def breed
+    @breed
+  end 
+  
+  
+end 
